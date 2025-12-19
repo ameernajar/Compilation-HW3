ce5949ed22231cd863ecf05acb13acbb5a50e66b
hmbojali <ali.abusaleh@campus.technion.ac.il>
2025-12-19 16:53:36 +0200

    Setup MyVisitor class and update main to use it for AST traversal

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include "output.hpp"
 #include "nodes.hpp"
+#include "myVisitor.hpp"
 
 // Extern from the bison-generated parser
 extern int yyparse();
 
 extern std::shared_ptr<ast::Node> program;
 
-int main() {
+int main()
+{
     // Parse the input. The result is stored in the global variable `program`
     yyparse();
 
-    // Print the AST using the PrintVisitor
-    output::PrintVisitor printVisitor;
-    program->accept(printVisitor);
+    MyVisitor visitor;
+    program->accept(visitor);
 }
--- myVisitor.cpp
+++ myVisitor.cpp
@@ -1,3 +1,108 @@
+#include "myVisitor.hpp"
 //
 // Created by amirn on 12/19/2025.
 //
+
+void MyVisitor::visit(ast::Num &node)
+{
+}
+
+void MyVisitor::visit(ast::NumB &node)
+{
+}
+
+void MyVisitor::visit(ast::String &node)
+{
+}
+
+void MyVisitor::visit(ast::Bool &node)
+{
+}
+
+void MyVisitor::visit(ast::ID &node)
+{
+}
+
+void MyVisitor::visit(ast::BinOp &node)
+{
+}
+
+void MyVisitor::visit(ast::RelOp &node)
+{
+}
+
+void MyVisitor::visit(ast::Not &node)
+{
+}
+
+void MyVisitor::visit(ast::And &node)
+{
+}
+
+void MyVisitor::visit(ast::Or &node)
+{
+}
+
+void MyVisitor::visit(ast::Type &node)
+{
+}
+
+void MyVisitor::visit(ast::Cast &node)
+{
+}
+
+void MyVisitor::visit(ast::ExpList &node)
+{
+}
+
+void MyVisitor::visit(ast::Call &node)
+{
+}
+
+void MyVisitor::visit(ast::Statements &node)
+{
+}
+
+void MyVisitor::visit(ast::Break &node)
+{
+}
+
+void MyVisitor::visit(ast::Continue &node)
+{
+}
+
+void MyVisitor::visit(ast::Return &node)
+{
+}
+
+void MyVisitor::visit(ast::If &node)
+{
+}
+
+void MyVisitor::visit(ast::While &node)
+{
+}
+
+void MyVisitor::visit(ast::VarDecl &node)
+{
+}
+
+void MyVisitor::visit(ast::Assign &node)
+{
+}
+
+void MyVisitor::visit(ast::Formal &node)
+{
+}
+
+void MyVisitor::visit(ast::Formals &node)
+{
+}
+
+void MyVisitor::visit(ast::FuncDecl &node)
+{
+}
+
+void MyVisitor::visit(ast::Funcs &node)
+{
+}
